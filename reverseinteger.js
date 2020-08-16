cdc061ba61e354c4403fd58a9ed1891ee5115cd7
Amatullah Mir <amatullahmir@Amatullahs-MBP.attlocal.net>
2020-08-15 22:52:58 -0500

    reverse integer formula in javascript

---
+++ reverseinteger.js
@@ -0,0 +1,12 @@
+return (
+    parseFloat(
+      num
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(num)
+  )
+}
+
+console.log(reversedNum(5879402)
